62c310ff3c5dd5d66a104a2c213d82ecb6bb1c0c
Ishrak-Adit07 <ishrak.adit07@gmail.com>
2025-05-25 13:48:44 +0600

    setup: authenticated connection to azure db

--- server/src/main/java/com/caresync/service/data/DataApplication.java
+++ server/src/main/java/com/caresync/service/data/DataApplication.java
@@ -1,13 +1,19 @@
 package com.caresync.service.data;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DataApplication {
+public class DataApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DataApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) {
+		System.out.println("CareSync Data Service");
 	}
 
 }
