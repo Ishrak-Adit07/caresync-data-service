19b1a900843d864a6e1e7b9d560b36c5aac97797
Ishrak-Adit07 <ishrak.adit07@gmail.com>
2025-06-01 18:22:05 +0600

    enum, request and data dto: location

---
+++ server/src/main/java/com/caresync/service/data/dtos/data/Location.java
@@ -0,0 +1,29 @@
+package com.caresync.service.data.dtos.data;
+
+
+import com.caresync.service.data.enums.LOCATION_TYPE;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Location {
+
+    private String id;
+
+    private LOCATION_TYPE locationType;
+
+    private String address;
+    private String thana;
+    private String po;
+
+    @NotBlank(message = "City cannot be blank")
+    private String city;
+
+    @NotBlank(message = "Postal code cannot be blank")
+    private String postalCode;
+
+    private String zoneId;
+}
---
+++ server/src/main/java/com/caresync/service/data/enums/LOCATION_TYPE.java
@@ -0,0 +1,5 @@
+package com.caresync.service.data.enums;
+
+public enum LOCATION_TYPE {
+    USER, DOCTOR, HOSPITAL
+}
